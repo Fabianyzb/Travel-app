88191deac3dd9accdc70fc10797824c44873ffbb
Fabian Yanez <fabian.yzb@gmail.com>
2024-05-30 15:53:53 -0400

    Navbar y Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-teal-500 text-2xl">
-        HOLA
+      <h1 className="font-bold text-teal-500 text-2xl5">
+        ESTA SERA UNA PAGINA WEB
       </h1>
     </>
   );
