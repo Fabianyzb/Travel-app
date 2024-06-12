899fdd14213639bd29da67bbea9727b81b964395
Fabian Yanez <fabian.yzb@gmail.com>
2024-06-12 08:46:53 -0400

    Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 // TypeScript: Definición de tipos para las props del componente
 type ButtonProps = {
   type: "button" | "submit";
@@ -6,9 +8,18 @@ type ButtonProps = {
   variant: "btn_dark_green";
 };
 
-// Componente de botón
+// Componente de botón de Login (icono del monito y la palabra)
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
-  return <button type="button"></button>;
+  return (
+    //el className de button es un string dinamico
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />} 
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
 };
 
 export default Button;
