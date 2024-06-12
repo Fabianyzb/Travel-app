df6a1386d2df763acd2dbd6f0b5ba0916cdf956f
Fabian Yanez <fabian.yzb@gmail.com>
2024-06-11 23:04:30 -0400

    progreso en Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar/>
-        <main className="relative overflow-hidden">
-          {children}
-        </main>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
 
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,33 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween 
-    mas-container padding-container relative z-30 py-5">
-        <Link href="/"> {/* cada link con su href */}
-            <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
-        </Link>
-
+    <nav
+      className="flexBetween 
+    max-container padding-container relative z-30 py-5"
+    >
+      <Link href="/">
+        {" "}
+        {/* cada link con su href */}
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      {/* lg:flex = visible en dispositivos grandes */}
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flex cursor-pointer pb-1.5 transition-all
+          hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
