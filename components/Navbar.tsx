063dcc3d4ac1edd26abadce43bf77b605bdb20bf
Fabian Yanez <fabian.yzb@gmail.com>
2024-06-12 11:16:08 -0400

    hamburguesa lista

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,31 @@
+"use client";
+
+import { useState } from "react";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav
-      className="flexBetween 
-    max-container padding-container relative z-30 py-5"
-    >
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        {" "}
-        {/* cada link con su href */}
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
-      {/* lg:flex = visible en dispositivos grandes */}
+
+      {/* Menú para dispositivos grandes */}
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
             key={link.key}
-            className="regular-16 text-gray-50 flex cursor-pointer pb-1.5 transition-all
-          hover:font-bold"
+            className="regular-16 text-gray-50 flex cursor-pointer pb-1.5 transition-all hover:font-bold hover:bg-gray-200"
           >
             {link.label}
           </Link>
@@ -35,13 +39,37 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
       </div>
+
+      {/* Icono de hamburguesa para dispositivos pequeños */}
       <Image
-        src="menu.svg"
+        src="/menu.svg"
         alt="menu"
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
-      ></Image>
+        onClick={toggleMenu}
+      />
+
+      {/* Menú desplegable para dispositivos pequeños con transición */}
+      <ul
+        className={`absolute top-full right-0 mt-2 w-48 bg-white shadow-lg rounded-lg lg:hidden transform transition-transform duration-300 ease-in-out ${
+          isMenuOpen
+            ? "translate-y-0 opacity-100"
+            : "-translate-y-4 opacity-0 pointer-events-none"
+        }`}
+      >
+        {NAV_LINKS.map((link) => (
+          <li key={link.key} className="border-b border-gray-200">
+            <Link
+              href={link.href}
+              className="block p-4 hover:bg-black hover:text-white" // Aquí se agregó hover:text-white y hover:bg-black
+              onClick={() => setIsMenuOpen(false)} // Cierra el menú al hacer clic en un enlace
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
